3ece0ca993dcb18a69dc4360c341e5386ae7c3ae
Joe <meanjoe45.acct@gmail.com>
2017-11-16 18:55:40 -0500

    Fix help menu wording

--- bin/dotenv-init.js
+++ bin/dotenv-init.js
@@ -16,7 +16,7 @@ program
   .usage('[options] <file ...>')
   .option('-s, --safe', '.env.example is also output for use with dotenv-safe')
   .option('-c, --comments', 'include comments when parsing for environment variables')
-  .option('-i, --ignore <patterns>', 'files to ignore as part of file patterns; overrides default (\'node_modules/**\', \'test/**\')', 'node_modules/**,test/**')
+  .option('-i, --ignore <patterns>', 'files to ignore as part of file patterns; overrides default (\'node_modules/**,test/**\')', 'node_modules/**,test/**')
   .option('-o, --output [level]', 'set the console output level [normal] (silent|normal|verbose)', /^(silent|normal|verbose)$/i, 'normal')
   .option('-O, --file-output [level]', 'set the file output level [normal] (minimal|normal|verbose)', /^(minimal|normal|verbose)$/i, 'normal')
   .option('--filename [name]', 'chose the name of the output file [.env]', '.env')
